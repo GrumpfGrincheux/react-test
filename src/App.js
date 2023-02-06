24416c2620df0889ae3a8449a3386e72f0a68916
GrumpfGrincheux <valentin-petitcolin@orange.fr>
2023-02-06 22:05:27 +0100

    import fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.scss";
-import { BrowserRouter, Outlet } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { QuotePage } from "./QuotePage";
 import { CounterPage } from "./CounterPage";
-import { Route, Routes } from "react-router-dom";
 import { Hello } from "./Hello";
 import { NavMenu } from "./NavMenu";
 import { LoginForm } from "./LoginForm";
@@ -10,30 +9,30 @@ import { Home } from "./Home";
 import { AuthenticatedRoute } from "./AuthenticatedRoute";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <NavMenu />
-        <Routes>
-          <Route path="/login" element={<LoginForm />} />
-          <Route
-            path="/"
-            element={
-              <AuthenticatedRoute>
-                <Outlet />
-              </AuthenticatedRoute>
-            }
-          >
-            <Route index element={<Home />} />
-            <Route path="quote" element={<QuotePage />} />
-            <Route path="counter" element={<CounterPage />} />
-            <Route path="hello" element={<Hello />} />
-          </Route>
-          <Route path="*" element={<p>Erreur 404 : Page non trouvée !</p>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<NavMenu />
+				<Routes>
+					<Route path="/login" element={<LoginForm />} />
+					<Route
+						path="/"
+						element={
+							<AuthenticatedRoute>
+								<Outlet />
+							</AuthenticatedRoute>
+						}
+					>
+						<Route index element={<Home />} />
+						<Route path="quote" element={<QuotePage />} />
+						<Route path="counter" element={<CounterPage />} />
+						<Route path="hello" element={<Hello />} />
+					</Route>
+					<Route path="*" element={<p>Erreur 404 : Page non trouvée !</p>} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
